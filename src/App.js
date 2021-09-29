f05d830fbe1a2f762b30584e1ab0a19628c0bd7c
Derrick Strong <derrickmstrong@gmail.com>
2021-09-29 06:18:09 -0500

    Access account state via useSelector hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const App = () => {
+    const account = useSelector((state) => state.account)
+    console.log('Account:', account)
     return (
         <div>
-            Hello from App
+            Account Balance: {account}
         </div>
     )
 }
